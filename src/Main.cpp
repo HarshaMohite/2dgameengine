b5292286d8b2ef67ea6e08d129e5569f178a22ee
Harsha Mohite <harsha_mohite@outlook.com>
2023-04-08 07:40:18 -0700

    Installed all dependencies and setup Makefile.

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char *args[]) {
+    // TODO:
+    // Add game class
+
+    std::cout << "Game is running..." << std::endl;
+    std::cout << "Test" << std::endl;
+    return 0;
+}
