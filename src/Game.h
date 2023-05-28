7e27347ecacc5c0bd6d82b1e22f92b32167c6a81
Harsha Mohite <harsha_mohite@outlook.com>
2023-05-27 17:17:52 -0700

    Initial implementation of the game class

---
+++ src/Game.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+/**
+ * This class handles the basic launch and structure of the game.
+*/
+class Game {
+    private:
+        /**
+         * If true, the game is running.
+        */    
+        bool isRunning;
+
+        /**
+         * Pointer to the running SDL window.
+        */
+        SDL_Window *window;
+
+        /**
+         * Pointer to the created SDL renderer.
+        */
+        SDL_Renderer *renderer;
+    public:
+        /**
+         * Called to bring up the world for play.
+        */
+        Game();
+
+        /**
+         * Called when this class is closed. Handles closing out the game.
+        */
+        ~Game();
+
+        /**
+         * @return bool isRunning, which states if the game is currently active.
+        */
+        bool IsRunning() const;
+
+        /**
+         * Initializes SDL, the game window, and any necessary components.
+         * @param width Window width.
+         * @param height Window height.
+        */
+        void Initialize(int width, int height);
+
+        /**
+         * Processes input every frame from the input buffer.
+        */
+        void ProcessInput();
+
+        /**
+         * Updates gameplay logic every frame.
+        */
+        void Update();
+
+        /**
+         * Handles rendering every frame.
+         * 
+         * Renders on the back buffer and then swaps with front buffer once rendered.
+        */
+        void Render();
+
+        /**
+         * Handles exiting the game.
+        */
+        void Destroy();
+};
